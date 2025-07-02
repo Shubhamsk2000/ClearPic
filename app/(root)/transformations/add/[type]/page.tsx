5a5f3784fb385a8c5b05e76931d8ba6007841a9e
Shubhamsk2000 <shubhamkondhalkar2003@gmail.com>
2025-07-02 19:48:39 +0530

    refactor: simplify parameter destructuring in AddTransformationTypePage

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -7,8 +7,8 @@ import { getUserById } from '@/lib/actions/user.actions';
 import { redirect } from 'next/navigation';
 
 
-const AddTransformationTypePage = async (props: SearchParamProps) => {
-  const { type } = await props.params;
+const AddTransformationTypePage = async ({ params }: SearchParamProps) => {
+  const { type } = params;
   const transformation = transformationTypes[type];
   const { userId } = await auth();
 
